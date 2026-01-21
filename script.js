d2511fbeff0d27e830c7fc8ceff0168c41de864d
Laxman Kumar <80440232+laxmankr72@users.noreply.github.com>
2026-01-21 16:49:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,14 @@
 
 function insert_Row() {
   let table = document.getElementById("sampleTable");
-	let newRow = document.createElement("tr");
-	newRow.innerHTML="<td>New Cell1</td><td>New Cell2</td>"
-	table.prepend(newRow);
+
+	console.log(table);
+
+	let firstRow = table.insertRow(0);
+	let cell1 = firstRow.insertCell(0);
+	let cell2 = firstRow.insertCell(1);
+
+	cell1.innerHTML = "New Cell1";
+	cell2.innerHTML = "New Cell2";
 }
+
